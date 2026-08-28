42a845bd21c4eb99a7857177d7244a309bc142d4
princellandelar1-glitch <princellandelar1@gmail.com>
2026-08-29 02:12:20 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const { app, BrowserWindow } = require('electron');
 require('./server.js'); // Pinapaandar ang server sa background
 
@@ -14,22 +13,4 @@ function createWindow() {
 
 app.whenReady().then(() => {
     createWindow();
-=======
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
-require('./server.js'); // Pinapaandar nito ang server sa background
-
-function createWindow() {
-    const win = new BrowserWindow({
-        width: 450,
-        height: 700,
-        title: "P and A App"
-    });
-
-    win.loadURL('http://localhost:3000');
-}
-
-app.whenReady().then(() => {
-    createWindow();
->>>>>>> b4af5385995fcfaeb8f81bc5e4e52adde2bae40e
 });
